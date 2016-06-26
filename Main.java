f324c1332a761ec39ab742c3bdd86401e56e7225
mansi910 <mansi09101993@gmail.com>
2016-06-26 20:41:54 +0530

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,105 +1,120 @@
-import java.util.Scanner;
-
-
-public class Calculator {
-
-
-    public static void main(String[] args) {
-
-        Scanner kb = new Scanner(System.in);
-
-        System.out.println("Simple Calculator");
-
-        System.out.println("\nHere are your options:");
-        System.out.println("\n1. Addition");
-        System.out.println("2. Subtraction");
-        System.out.println("3. Division");
-        System.out.println("4. Multiplication");
-
-        System.out.print("\nWhat would you like to do?: ");
-        int choice = kb.nextInt();
-        System.out.println();
-
-
-        if (choice == 1){
-            addition();
-        }
-        else if (choice == 2){
-            subtraction();
-        }
-        else if (choice == 3){
-            division();
-        }
-        else if (choice == 4){
-            multiplication();
-        }
-
-        System.out.println();
-        kb.close();
-    }
-
-    public static void addition(){
-
-        int nOne, nTwo;
-        Scanner kb = new Scanner(System.in);
-
-        System.out.println("Addition");
-
-        System.out.print("\nFirst Number: ");
-        nOne = kb.nextInt();
-
-        System.out.print("\nSecond Number: ");
-        nTwo = kb.nextInt();
-
-        kb.close();
-        System.out.println("\nSum: " + nOne + " + " + nTwo + " = " + (nOne + nTwo));
-    }
-
-    public static void subtraction(){
-        int nOne, nTwo;
-        Scanner kb = new Scanner(System.in);
-
-        System.out.println("Subtraction");
-
-        System.out.print("\nFirst Number: ");
-        nOne = kb.nextInt();
-
-        System.out.print("\nSecond Number: ");
-        nTwo = kb.nextInt();
-
-        kb.close();
-        System.out.println("\nSum: " + nOne + " - " + nTwo + " = " + (nOne - nTwo));
-    }
-
-    public static void division(){
-        int nOne, nTwo;
-        Scanner kb = new Scanner(System.in);
-
-        System.out.println("Division");
-
-        System.out.print("\nFirst Number: ");
-        nOne = kb.nextInt();
-
-        System.out.print("\nSecond Number: ");
-        nTwo = kb.nextInt();
-
-        kb.close();
-        System.out.println("\nSum: " + nOne + " / " + nTwo + " = " + (nOne / nTwo));
-    }
-
-    public static void multiplication(){
-        int nOne, nTwo;
-        Scanner kb = new Scanner(System.in);
-
-        System.out.println("Multiplication");
-
-        System.out.print("\nFirst Number: ");
-        nOne = kb.nextInt();
-
-        System.out.print("\nSecond Number: ");
-        nTwo = kb.nextInt();
-
-        kb.close();
-        System.out.println("\nSum: " + nOne + " x " + nTwo + " = " + (nOne * nTwo));
-    }
+import java.awt.*;
+import java.awt.event.*;
+import java.applet.*;
+ 
+/* 
+<applet code="Cal" width=300 height=300>
+</applet>
+*/
+ 
+public class Cal extends Applet
+implements ActionListener
+{
+	String msg=" ";
+	int v1,v2,result;
+	TextField t1;
+	Button b[]=new Button[10];
+	Button add,sub,mul,div,clear,mod,EQ;
+	char OP;
+	public void init()
+	{
+		Color k=new Color(120,89,90);
+		setBackground(k);
+		t1=new TextField(10);
+		GridLayout gl=new GridLayout(4,5);
+		setLayout(gl);
+		for(int i=0;i<10;i++)
+		{
+			b[i]=new Button(""+i);
+		}
+		add=new Button("add");
+		sub=new Button("sub");
+		mul=new Button("mul");
+		div=new Button("div");
+		mod=new Button("mod");
+		clear=new Button("clear");
+		EQ=new Button("EQ");
+		t1.addActionListener(this);
+		add(t1);
+		for(int i=0;i<10;i++)
+		{
+			add(b[i]);
+		}
+		add(add);
+		add(sub);
+		add(mul);
+		add(div);
+		add(mod);
+		add(clear);
+		add(EQ);
+		for(int i=0;i<10;i++)
+		{
+			b[i].addActionListener(this);
+		}
+		add.addActionListener(this);
+		sub.addActionListener(this);
+		mul.addActionListener(this);
+		div.addActionListener(this);
+		mod.addActionListener(this);
+		clear.addActionListener(this);
+		EQ.addActionListener(this);
+	}
+ 
+	public void actionPerformed(ActionEvent ae)
+	{
+		String str=ae.getActionCommand();
+		char ch=str.charAt(0);
+		if ( Character.isDigit(ch))
+		t1.setText(t1.getText()+str);
+		else
+		if(str.equals("add"))
+		{
+			v1=Integer.parseInt(t1.getText());
+			OP='+';
+			t1.setText("");
+		}
+		else if(str.equals("sub"))
+		{
+			v1=Integer.parseInt(t1.getText());
+			OP='-';
+			t1.setText("");
+		}
+		else if(str.equals("mul"))
+		{
+			v1=Integer.parseInt(t1.getText());
+			OP='*';
+			t1.setText("");
+		}
+		else if(str.equals("div"))
+		{
+			v1=Integer.parseInt(t1.getText());
+			OP='/';
+			t1.setText("");
+		}
+		else if(str.equals("mod"))
+		{
+			v1=Integer.parseInt(t1.getText());
+			OP='%';
+			t1.setText("");
+		}
+		if(str.equals("EQ"))
+		{
+			v2=Integer.parseInt(t1.getText());
+			if(OP=='+')
+				result=v1+v2;
+			else if(OP=='-')
+				result=v1-v2;
+			else if(OP=='*')
+				result=v1*v2;
+			else if(OP=='/')
+				result=v1/v2;
+			else if(OP=='%')
+				result=v1%v2;
+			t1.setText(""+result);
+		}	
+		if(str.equals("clear"))
+		{
+			t1.setText("");
+		}
+	}
 }
