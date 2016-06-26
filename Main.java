d7d5422f9c4b23c2fbb0ed2a0f7403e24a6013a2
mansi910 <mansi09101993@gmail.com>
2016-06-26 17:24:00 +0530

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,9 +1,9 @@
 public class Main {
    public static void main(String args[]) {
-      String str = "this is Java";
-      System.out.println(removeCharAt(str, 3));
+      String str = "this is Java";//this statemnet prints
+      System.out.println(removeCharAt(str, 3))
    }
    public static String removeCharAt(String s, int pos) {
-      return s.substring(0, pos) + s.substring(pos + 1);
+      return s.substring(0, pos) + s.substring(pos + 1)//substring
    }
 }
