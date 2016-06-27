d44f78d13833905bfb7ad8264a5771a215271b67
mansi910 <mansi09101993@gmail.com>
2016-06-27 13:30:04 +0530

    Update Cal.java

--- Cal.java
+++ Cal.java
@@ -1,120 +1,74 @@
-import java.awt.*;
-import java.awt.event.*;
-import java.applet.*;
+//This is a java program to sort numbers using randomized quick sort
+import java.util.Random;
  
-/* 
-<applet code="Cal" width=300 height=300>
-</applet>
-*/
- 
-public class Cal extends Applet
-implements ActionListener
+public class Randomized_Quick_Sort 
 {
-	String msg=" ";
-	int v1,v2,result;
-	TextField t1;
-	Button b[]=new Button[10];
-	Button add,sub,mul,div,clear,mod,EQ;
-	char OP;
-	public void init()
-	{
-		Color k=new Color(120,89,90);
-		setBackground(k);
-		t1=new TextField(10);
-		GridLayout gl=new GridLayout(4,5);
-		setLayout(gl);
-		for(int i=0;i<10;i++)
-		{
-			b[i]=new Button(""+i);
-		}
-		add=new Button("add");
-		sub=new Button("sub");
-		mul=new Button("mul");
-		div=new Button("div");
-		mod=new Button("mod");
-		clear=new Button("clear");
-		EQ=new Button("EQ");
-		t1.addActionListener(this);
-		add(t1);
-		for(int i=0;i<10;i++)
-		{
-			add(b[i]);
-		}
-		add(add);
-		add(sub);
-		add(mul);
-		add(div);
-		add(mod);
-		add(clear);
-		add(EQ);
-		for(int i=0;i<10;i++)
-		{
-			b[i].addActionListener(this);
-		}
-		add.addActionListener(this);
-		sub.addActionListener(this);
-		mul.addActionListener(this);
-		div.addActionListener(this);
-		mod.addActionListener(this);
-		clear.addActionListener(this);
-		EQ.addActionListener(this);
-	}
+    public static int N = 20;
+    public static int[] sequence = new int[N];
  
-	public void actionPerformed(ActionEvent ae)
-	{
-		String str=ae.getActionCommand();
-		char ch=str.charAt(0);
-		if ( Character.isDigit(ch))
-		t1.setText(t1.getText()+str);
-		else
-		if(str.equals("add"))
-		{
-			v1=Integer.parseInt(t1.getText());
-			OP='+';
-			t1.setText("");
-		}
-		else if(str.equals("sub"))
-		{
-			v1=Integer.parseInt(t1.getText());
-			OP='-';
-			t1.setText("");
-		}
-		else if(str.equals("mul"))
-		{
-			v1=Integer.parseInt(t1.getText());
-			OP='*';
-			t1.setText("");
-		}
-		else if(str.equals("div"))
-		{
-			v1=Integer.parseInt(t1.getText());
-			OP='/';
-			t1.setText("");
-		}
-		else if(str.equals("mod"))
-		{
-			v1=Integer.parseInt(t1.getText());
-			OP='%';
-			t1.setText("");
-		}
-		if(str.equals("EQ"))
-		{
-			v2=Integer.parseInt(t1.getText());
-			if(OP=='+')
-				result=v1+v2;
-			else if(OP=='-')
-				result=v1-v2;
-			else if(OP=='*')
-				result=v1*v2;
-			else if(OP=='/')
-				result=v1/v2;
-			else if(OP=='%')
-				result=v1%v2;
-			t1.setText(""+result);
-		}	
-		if(str.equals("clear"))
-		{
-			t1.setText("");
-		}
-	}
+    public static void QuickSort(int left, int right) 
+    {
+        if (right - left <= 0)
+            return;
+        else 
+        {
+            Random rand = new Random();
+            int pivotIndex = left + rand.nextInt(right - left + 1);
+            swap(pivotIndex, right);
+ 
+            int pivot = sequence[right];
+            int partition = partitionIt(left, right, pivot);
+            QuickSort(left, partition - 1);
+            QuickSort(partition + 1, right);
+        }
+    }
+ 
+    public static int partitionIt(int left, int right, long pivot) 
+    {
+        int leftPtr = left - 1;
+        int rightPtr = right;
+        while (true) 
+        {
+            while (sequence[++leftPtr] < pivot)
+                ;
+            while (rightPtr > 0 && sequence[--rightPtr] > pivot)
+                ;
+ 
+            if (leftPtr >= rightPtr)
+                break;
+            else
+                swap(leftPtr, rightPtr);
+        }
+        swap(leftPtr, right);
+        return leftPtr;
+    }
+ 
+    public static void swap(int dex1, int dex2) 
+    {
+        int temp = sequence[dex1];
+        sequence[dex1] = sequence[dex2];
+        sequence[dex2] = temp;
+    }
+ 
+    static void printSequence(int[] sorted_sequence) 
+    {
+        for (int i = 0; i < sorted_sequence.length; i++)
+            System.out.print(sorted_sequence[i] + " ");
+    }
+ 
+    public static void main(String args[]) 
+    {
+        System.out
+                .println("Sorting of randomly generated numbers using RANDOMIZED QUICK SORT");
+        Random random = new Random();
+ 
+        for (int i = 0; i < N; i++)
+            sequence[i] = Math.abs(random.nextInt(100));
+ 
+        System.out.println("\nOriginal Sequence: ");
+        printSequence(sequence);
+        System.out.println("\nSorted Sequence: ");
+        QuickSort(0, N - 1);
+        printSequence(sequence);
+    }
 }
